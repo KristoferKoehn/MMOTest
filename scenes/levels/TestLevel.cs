082c960006ad7317c5f40b58bae070818fdc106a
Kristofer Koehn <fernandoprofitt@gmail.com>
2024-02-08 11:19:28 -0800

    changing node names

--- scenes/levels/TestLevel.cs
+++ scenes/levels/TestLevel.cs
@@ -139,12 +139,13 @@ public partial class TestLevel : Node3D
         if(host)
         {
             TestAbility fb = GD.Load<PackedScene>("res://scenes/abilities/TestAbility.tscn").Instantiate<TestAbility>();
+            
             fb.ApplyHost(host);
             Vector3 position = new Vector3(args[0], args[1], args[2]);
             Vector3 velocity = new Vector3(args[3], args[4], args[5]);
             fb.Position = position + velocity * 2;
             fb.LinearVelocity = velocity * 20;
-            GetNode<Node>("AbilityModels").AddChild(fb);
+            GetNode<Node>("AbilityModels").AddChild(fb, forceReadableName: true);
             GD.Print("server detected user activated ability " + SceneName + " with " + args.Length + " length arg");
             GD.Print("pos x" + args[0]);
             GD.Print("pos y" + args[1]);
