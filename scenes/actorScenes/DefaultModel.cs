2380ad6bbda423265d57370cce2cae6c365ad7b6
kkoehn <kristofer.koehn@wsu.edu>
2024-02-19 14:56:22 -0800

    made puppet model the default model

--- scenes/actorScenes/DefaultModel.cs
+++ scenes/actorScenes/DefaultModel.cs
@@ -9,17 +9,17 @@ public partial class DefaultModel : CharacterBody3D
 
     public override void _EnterTree()
     {
-        if (TrackingPeerId == SimulationPeerId)
-        {
-            this.Visible = false;
-        }
+
     }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-
-	}
+        if (TrackingPeerId == SimulationPeerId)
+        {
+            this.Visible = false;
+        }
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
