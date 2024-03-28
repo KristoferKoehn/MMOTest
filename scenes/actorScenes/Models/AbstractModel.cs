af020f1aa920659fef333231469789c3599f75b1
kkoehn <kristofer.koehn@wsu.edu>
2024-03-27 19:08:01 -0700

    we're getting there

--- scenes/actorScenes/Models/AbstractModel.cs
+++ scenes/actorScenes/Models/AbstractModel.cs
@@ -4,9 +4,10 @@ using System;
 public abstract partial class AbstractModel : CharacterBody3D
 {
 	public bool IsDead { get; set; } = false;
+    public long SimulationPeerId { get; set; } = -2;
 
-	// Called when the node enters the scene tree for the first time.
-	public abstract void AttachController(AbstractController controller);
+    // Called when the node enters the scene tree for the first time.
+    public abstract void AttachController(AbstractController controller);
 	public abstract void SetTrackingPeerId(long peerId);
 	public abstract void SetActorID(int actorId);
 	public abstract int GetActorID();
