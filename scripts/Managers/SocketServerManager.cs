7e4618675ee97a3d3b203523a493f17d9aac871e
kkoehn <kristofer.koehn@wsu.edu>
2024-04-04 21:48:50 -0700

    test

--- scripts/Managers/SocketServerManager.cs
+++ scripts/Managers/SocketServerManager.cs
@@ -1,70 +1,100 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
-public partial class SocketServerManager : Node
+namespace Managers.SocketServerManager
 {
-    int inport = 9002;
-    int outport = 9003;
 
+    class PendingPeer
+    {
+        public ulong ConnectTime;
+        public StreamPeerTcp tcp;
+        public StreamPeer connection;
+        public WebSocketPeer ws;
 
-    private SocketServerManager() { }
-    private static SocketServerManager instance = null;
-    TcpServer TCPin = null;
-    WebSocketPeer wsp = null;
-
-    public static SocketServerManager GetInstance() { 
-        if (instance == null)
+        PendingPeer(StreamPeerTcp p_tcp)
         {
-            instance = new SocketServerManager();
-            instance.Name = "SocketServerManager";
-            GameLoop.Root.GetNode<MainLevel>("GameLoop/MainLevel").AddChild(instance);
+            tcp = p_tcp;
+            connection = p_tcp;
+            ConnectTime = Time.GetTicksMsec();
         }
-
-        return instance;
     }
 
-    public override void _Ready()
+
+
+    public partial class SocketServerManager : Node
     {
-        wsp = new WebSocketPeer();
-        TCPin = new TcpServer();
-        TCPin.Listen(9002);
-    }
+        int inport = 9002;
+        int outport = 9003;
 
-    public override void _Process(double delta)
-    {
-        wsp.Poll();
 
-        if (TCPin.IsConnectionAvailable())
+        private SocketServerManager() { }
+        private static SocketServerManager instance = null;
+        TcpServer TCPin = null;
+        WebSocketPeer wsp = null;
+
+
+
+        public static SocketServerManager GetInstance()
         {
-            StreamPeerTcp stream = TCPin.TakeConnection();
-            GD.Print("web client connected from" + stream.GetConnectedHost());
-            wsp.AcceptStream(stream);
-        }
-
-        WebSocketPeer.State state = wsp.GetReadyState();
-
-        if (state == WebSocketPeer.State.Open)
-        {
-            while(wsp.GetAvailablePacketCount() > 0)
+            if (instance == null)
             {
-                byte[] msg = wsp.GetPacket();
-                GD.Print(msg.ToString());
+                instance = new SocketServerManager();
+                instance.Name = "SocketServerManager";
+                GameLoop.Root.GetNode<MainLevel>("GameLoop/MainLevel").AddChild(instance);
+            }
+
+            return instance;
+        }
+
+        public override void _Ready()
+        {
+            wsp = new WebSocketPeer();
+            TCPin = new TcpServer();
+            TCPin.Listen(9002);
+        }
+
+        public override void _Process(double delta)
+        {
+            wsp.Poll();
+
+            if (TCPin.IsConnectionAvailable())
+            {
+                StreamPeerTcp stream = TCPin.TakeConnection();
+                GD.Print("web client connected from" + stream.GetConnectedHost());
+                wsp.AcceptStream(stream);
+            }
+
+            WebSocketPeer.State state = wsp.GetReadyState();
+
+            if (state == WebSocketPeer.State.Open)
+            {
+                while (wsp.GetAvailablePacketCount() > 0)
+                {
+                    byte[] msg = wsp.GetPacket();
+                    GD.Print(msg.ToString());
+                }
+            }
+
+            if (state == WebSocketPeer.State.Closed)
+            {
+                wsp.Close();
+            }
+
+            if (state == WebSocketPeer.State.Closing)
+            {
+                //do nothing?
+            }
+            if (state == WebSocketPeer.State.Connecting)
+            {
+                wsp.Poll();
             }
         }
 
-        if(state == WebSocketPeer.State.Closed)
-        {
-            wsp.Close();
-        }
-
-        if(state == WebSocketPeer.State.Closing)
-        {
-            //do nothing?
-        }
-        if(state == WebSocketPeer.State.Connecting)
-        {
-            wsp.Poll();
-        }
-
     }
+
+
+
+
 }
+
